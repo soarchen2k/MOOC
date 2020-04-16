5e9bfa217ed7c3b6d98c0ae3eab57fe32191faaf
soarchen <chenchong.blog@gmail.com>
2020-04-16 00:29:08 -0400

    iMac

---
+++ Part2/src/ca/monor/week10/w10_31_FarmSimulator/farmsimulator/Main.java
@@ -0,0 +1,4 @@
+package ca.monor.week10.w10_31_FarmSimulator.farmsimulator;
+
+public class Main {
+}
---
+++ Part2/src/ca/monor/week10/w10_31_FarmSimulator/farmsimulator/Milkable.java
@@ -0,0 +1,5 @@
+package ca.monor.week10.w10_31_FarmSimulator.farmsimulator;
+
+public interface Milkable {
+    double milk();
+}
